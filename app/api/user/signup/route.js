489661c9af4313c8d15724870fc6ecc40fb43b67
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-03-28 19:09:35 +0530

    Checking route.js

--- app/api/user/signup/route.js
+++ app/api/user/signup/route.js
@@ -4,21 +4,21 @@ import bcrypt from "bcrypt"
 export async function POST(request){
     let body = await request.json()
     const {email,username,password} = body
-    let userExists = await Users.findOne({email})
-    if(userExists){
-        return NextResponse.json({"message":"User already Exists"})
-    }
-    userExists = await Users.findOne({username})
-    if(userExists){
-        return NextResponse.json({"message":"Username exists"})
-    }
-    const hashedPassword = await bcrypt.hash(password,10)
-    let createUser = await Users.create({
-        email,username,password:hashedPassword
-    })
-    if(createUser){
-        createUser.password = undefined
-        return NextResponse.json({"message":"Success","user":createUser})
-    }
-    return NextResponse.json({"message":"Something went wrong"})
+    // let userExists = await Users.findOne({email})
+    // if(userExists){
+    //     return NextResponse.json({"message":"User already Exists"})
+    // }
+    // userExists = await Users.findOne({username})
+    // if(userExists){
+    //     return NextResponse.json({"message":"Username exists"})
+    // }
+    // const hashedPassword = await bcrypt.hash(password,10)
+    // let createUser = await Users.create({
+    //     email,username,password:hashedPassword
+    // })
+    // if(createUser){
+    //     createUser.password = undefined
+    //     return NextResponse.json({"message":"Success","user":createUser})
+    // }
+    return NextResponse.json({"message":"Something went wrong","email",email})
 }
