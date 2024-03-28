3a577a2ab255f894df991c1d1a5e751a1dd0d2ed
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-03-29 01:14:04 +0530

    added alert

--- app/ui/signin/SigninComponent.js
+++ app/ui/signin/SigninComponent.js
@@ -18,8 +18,7 @@ const SigninComponent = () => {
         console.log(body);
     };
 
-    return (
-        <div className="flex justify-center items-center h-screen">
+    return (<>
             {show && <Alert variant="danger" onClose={() => setShow(false)} dismissible>
             <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
             <p>
@@ -28,6 +27,7 @@ const SigninComponent = () => {
               Cras mattis consectetur purus sit amet fermentum.
             </p>
           </Alert>}
+        <div className="flex justify-center items-center h-screen">
             <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -65,6 +65,7 @@ const SigninComponent = () => {
                 </div>
             </form>
         </div>
+        </>
     );
 };
 
