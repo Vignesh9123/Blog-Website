79135d4909c838047c14c32616c5909825171ed8
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-03-28 18:28:59 +0530

    Update dbConnect.js

--- app/actions/dbConnect.js
+++ app/actions/dbConnect.js
@@ -2,7 +2,7 @@
 import mongoose from "mongoose";
 import Comments from "@/app/models/commentsSchema"
 export default async function dbConnect(){
-mongoose.connect(process.env.DATABASE_URL,{dbName:"Sample_comm"}).then(async()=>{
+mongoose.connect(process.env.MONGODB_URI,{dbName:"Sample_comm"}).then(async()=>{
     console.log("Connected");
 })
 .catch(e=>{console.log("Mongoose error",e);})
