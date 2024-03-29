e1f879776c76384f3c041b7bd4089a3490a39c56
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-03-29 16:44:42 +0530

    updated commentsShimmer

--- app/ui/Loading.js
+++ app/ui/Loading.js
@@ -1,7 +1,7 @@
 export const CommentsShimmer = ()=>{
   return(
    <> 
-<div role="status" class="max-w-md p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+<div role="status" class="max-w-md p-4 space-y-4  animate-pulse">
     <div class="flex items-center justify-between">
         <div>
             <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
