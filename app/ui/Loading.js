54238527f34867a68b329f91c5522707ddce71fd
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-03-29 17:07:42 +0530

    update shimmer

--- app/ui/Loading.js
+++ app/ui/Loading.js
@@ -2,20 +2,8 @@ export const CommentsShimmer = ()=>{
   return(
    <> 
 <div role="status" class="w-full max-h-screen p-4 space-y-4  animate-pulse">
-    <div class="flex items-center justify-between">
-        <div>
-            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-        </div>
-        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-    </div>
-    <div class="flex items-center justify-between pt-4">
-        <div>
-            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-        </div>
-        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-    </div>
+    
+    
     <div class="flex items-center justify-between pt-4">
         <div>
             <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
