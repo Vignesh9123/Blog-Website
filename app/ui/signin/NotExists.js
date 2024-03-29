9882dde6424c9eca1bed78ff61e5409526dd90e3
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-03-29 13:09:43 +0530

    updated

--- app/ui/signin/NotExists.js
+++ app/ui/signin/NotExists.js
@@ -14,7 +14,7 @@ const Alert = () => {
             <i className="fas fa-bell" />
           </span>
           <span className="inline-block align-middle mr-8">
-            <b className="capitalize">User does not exist</b> Please <Link href="/user/signup">Sign up</Link>
+            <b className="capitalize">User does not exist</b> Please <Link className="underline" href="/user/signup">Sign up</Link>
           </span>
           <button
             className="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none"
