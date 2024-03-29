ada8d870e045862fa5de5acb9275c265191c047d
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-03-29 13:07:45 +0530

    updated not-found

--- app/not-found.js
+++ app/not-found.js
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className='flex flex-col justify-center h-screen items-center'>
       <h2 className='text-xl font-bold text-center'>Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <a href="/">Return Home</a>
     </div>
   )
 }
